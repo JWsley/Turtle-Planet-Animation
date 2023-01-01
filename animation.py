bbb77b7c4f5ba754cfc7252f91ae59fccbdb9fbd
“JWsley” <jonathanwleycijani@gmail.com>
2022-12-31 21:07:09 -0300

    first commit

---
+++ animation.py
@@ -0,0 +1,30 @@
+import turtle
+
+
+turtle.title("Planet animation")
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor('black')
+t.pencolor('blue')                                
+
+x = 0
+y = 0
+t.speed(0)
+t.penup()
+t.goto(0,200)                                                              
+t.pendown()
+while True:
+
+    t.forward(x)
+    t.right(y)
+    x+=3
+    y+=1
+
+    if y == 210:
+        break
+    t.hideturtle()
+
+
+turtle.done()
